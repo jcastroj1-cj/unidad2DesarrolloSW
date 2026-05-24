7b51ae717b71dc8e355c25727238779b14330b3e
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-05-24 15:54:46 -0500

    guia 6, agregando datos de mi proyecto asignado

---
+++ unidad2DesarrolloSW/src/main/java/com/crud/celulares/unidad2DesarrolloSW/Celulares/Celulares.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.crud.celulares.unidad2DesarrolloSW.Celulares;
+
+import lombok.Data;
+/**
+ *
+ * @author Jean Carlos
+ */
+@Data
+public class Celulares {
+
+    public static Celulares c;
+
+    //PROPIEDADES
+    private String marca;
+    private String imei;
+    private String pulgadas;
+    private String megapixeles;
+    private String ram;
+    private String almacenimientoPrincial;          
+    private String almacenimientoSegundario;
+    private String SistemaOperativo;
+    private String Operador;
+    private String TecnologiaBanda;
+    private String  Wifi;
+    private String Blueto;
+    private String camara;
+    private String marcaCpu;
+    private String VelocidadCpu;
+    private String nfc;
+    private String huella;
+    private String ir;
+    private String resistenteAgua;
+    private int CantidadSim;
+
+
+}
--- unidad2DesarrolloSW/src/main/java/com/crud/celulares/unidad2DesarrolloSW/ControladorInicio.java
+++ unidad2DesarrolloSW/src/main/java/com/crud/celulares/unidad2DesarrolloSW/ControladorInicio.java
@@ -5,20 +5,56 @@
 package com.crud.celulares.unidad2DesarrolloSW;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController; 
+import com.crud.celulares.unidad2DesarrolloSW.Celulares.Celulares;
+import static com.crud.celulares.unidad2DesarrolloSW.Celulares.Celulares.c;
 /**
  *
  * @author Jean Carlos
  */
-@RestController
+
+@Controller
 @Slf4j
 public class ControladorInicio {
+    @Value("${index.mensaje}")
+    String dato; 
     @GetMapping("/")
-    public String inicio(){
-        log.info("Ejejcutar el controlador Inicio");
-        return "Este es el inicio v2";
+    public String inicio(Model modelo){
+        String mensaje ="Saludos desde Spring MVC";
+        modelo.addAttribute("mensaje", mensaje);
+        modelo.addAttribute("dato", dato);
+        Celulares.c = new Celulares();
+        c.setMarca("Samsung");
+        c.setImei("000000000");
+        c.setPulgadas("7");
+        c.setMegapixeles("100x24");
+        c.setRam("4Gb");
+        c.setAlmacenimientoPrincial("120Gb");
+        c.setAlmacenimientoSegundario("N/A");
+        c.setSistemaOperativo("Andoid 14");
+        c.setOperador("Claro");
+        c.setTecnologiaBanda("4G");
+        c.setWifi("Si");
+        c.setBlueto("si");
+        c.setCamara("Si");
+        c.setMarcaCpu("ntel");
+        c.setVelocidadCpu("2.4 Ghz");
+        c.setNfc("Si");
+        c.setHuella("Si");
+        c.setIr("Si");
+        c.setResistenteAgua("NO");
+        c.setCantidadSim(2);
+
+        
+                
+       modelo.addAttribute("Algun",c);
+        log.info("Ejejcutar el controlador Inicio MVC");
+        return "index";
     }    
             
+    
             
 }
