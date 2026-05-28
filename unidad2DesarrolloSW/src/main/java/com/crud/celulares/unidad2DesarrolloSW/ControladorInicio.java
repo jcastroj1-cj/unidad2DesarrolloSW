4bfdc7c3f44f2ec8df3a5a4f06e04cb1059f8ec3
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-05-27 21:06:02 -0500

    guia 7

--- unidad2DesarrolloSW/src/main/java/com/crud/celulares/unidad2DesarrolloSW/ControladorInicio.java
+++ unidad2DesarrolloSW/src/main/java/com/crud/celulares/unidad2DesarrolloSW/ControladorInicio.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.crud.celulares.unidad2DesarrolloSW;
-
+import java.util.List;
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,13 +27,26 @@ public class ControladorInicio {
         String mensaje = "Saludo desde Spring MVC";
         modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
-        Usuario u = new Usuario();
-        u.setCedula("1234");
-        u.setClave("Abcd");
-        u.setNombre("Jean Carlos");
-        u.setNombre("jeanCarlos@orueba.com");
-        modelo.addAttribute("alguien", u);
-        
+        Usuario u1 = new Usuario();
+        u1.setCedula("1234");
+        u1.setClave("Abcd");
+        u1.setNombre("Jean Carlos");
+        u1.setNombre("jeanCarlos@orueba.com");
+        modelo.addAttribute("alguien", u1);
+        Usuario u2 = new Usuario();
+        u2.setCedula("1234");
+        u2.setClave("Abcd");
+        u2.setNombre("Jean Carlos");
+        u2.setNombre("jeanCarlos@orueba.com");
+        modelo.addAttribute("alguien", u1);
+        Usuario u3 = new Usuario();
+        u3.setCedula("1234");
+        u3.setClave("Abcd");
+        u3.setNombre("Jean Carlos");
+        u3.setNombre("jeanCarlos@orueba.com");
+        modelo.addAttribute("alguien", u3);        
+        List<Usuario> listaUsuarios = Arrays.asList(u2,u3);
+        modelo.addAttribute("usuarios",listaUsuarios);
         log.info("Ejecutar el controlador de inicio");
         return "Index";
     }
