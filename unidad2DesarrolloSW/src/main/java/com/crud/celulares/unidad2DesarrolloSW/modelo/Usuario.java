079404e48c8826df98933a091b1b9af712d4e9ae
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-05-27 20:39:00 -0500

    cambiando la estructura del proyecto por error

---
+++ unidad2DesarrolloSW/src/main/java/com/crud/celulares/unidad2DesarrolloSW/modelo/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.crud.celulares.unidad2DesarrolloSW.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Jean Carlos
+ */
+@Data
+public class Usuario {
+    //PROPIEDADES
+    private String cedula;
+    private String clave;
+    private String nombre;
+    private String email;
+    
+}
